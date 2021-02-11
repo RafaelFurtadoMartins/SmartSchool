8c85d8c434342d95908967a9800c72f74fb8dc7b
RafaelFurtadoMartins <rafael.martins2231@gmail.com>
2021-02-11 16:25:16 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfessoresComponent } from './professores/professores.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'professores', component: ProfessoresComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
