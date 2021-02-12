d69d6d1953dbebacd9cd919e67321cfe61c85afa
RafaelFurtadoMartins <rafael.martins2231@gmail.com>
2021-02-12 09:40:26 -0300

    Update alunos.component.ts

--- src/app/alunos/alunos.component.ts
+++ src/app/alunos/alunos.component.ts
@@ -6,15 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alunos.component.css']
 })
 export class AlunosComponent implements OnInit {
-  titulo = 'Alunos';
-  alunos = [
-    'aluno 1',
-    'aluno 2',
-    'aluno 3',
-    'aluno 4',
-    'aluno 5',
-    'aluno 6',
-  ]
+ 
 
   constructor() { }
 
