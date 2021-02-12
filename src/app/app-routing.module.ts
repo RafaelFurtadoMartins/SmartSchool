760e3647d57b3b0c223c0a8ea7c9b200adf0d94c
RafaelFurtadoMartins <rafael.martins2231@gmail.com>
2021-02-12 08:56:13 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AlunosComponent } from './alunos/alunos.component';
 import { ProfessoresComponent } from './professores/professores.component';
 
 const routes: Routes = [
-  { path: 'professores', 
-  component: ProfessoresComponent }
+  { path: 'professores', component: ProfessoresComponent },
+  { path: 'Alunos', component: AlunosComponent }
 ];
 
 @NgModule({
